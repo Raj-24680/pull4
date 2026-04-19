192424d9de5833176c1eb19ce7780ca42ccbdb38
Raj-24680 <rrrj24680@gmail.com>
2026-04-19 17:16:03 +0530

    Modify App to find the greatest of three numbers
    Updated the program to find the greatest of three numbers instead of calculating the sum of two.

--- App.java
+++ App.java
@@ -1,10 +1,17 @@
-
 public class App {
     public static void main(String[] args) {
-        int a = 50;
-        int b = 10;
-        int sum = a + b;
 
-        System.out.println("Sum = " + sum);
+        int a = 10;
+        int b = 25;
+        int c = 15;
+
+        if (a >= b && a >= c) {
+            System.out.println("Greatest number is: " + a);
+        } else if (b >= a && b >= c) {
+            System.out.println("Greatest number is: " + b);
+        } else {
+            System.out.println("Greatest number is: " + c);
+        }
+
     }
 }
